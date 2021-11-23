01aa02b33e70a1b8bc613108421b7bb278336386
thuando <thuan14121999@gmail.com>
2021-11-23 19:13:10 +0700

    feat: try logging with zap

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module unit-test
+module unit_test
 
 go 1.16
 
@@ -6,6 +6,9 @@ require (
 	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
 	github.com/jinzhu/now v1.1.3 // indirect
 	github.com/onsi/gomega v1.17.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
 	golang.org/x/mod v0.5.1 // indirect
 	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
 	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
---
+++ zap_logging.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Name  string
+	Price uint
+}
+
+func hello(name string) string {
+	if name == "" {
+		return "Hello, what's your name?"
+	} else {
+		return fmt.Sprintf("Hello %s", name)
+	}
+}
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, "Hello world")
+}
+
+func New(a string) string {
+	return a
+}
+
+var logger *zap.Logger
+
+func main() {
+	InitLogger()
+}
+
+type foo struct {
+	One string
+	Two string
+}
+
+func InitLogger() {
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.EncoderConfig.EncodeDuration = zapcore.StringDurationEncoder
+	config.EncoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+
+	logger, err := config.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Error("Logger initialized")
+	logger.Debug("test", zap.Any("foo", foo{One: "one", Two: "two"}))
+	logger.Info("test", zap.Any("foo", foo{One: "one", Two: "two"}))
+	logger.Warn("test", zap.Any("foo", foo{One: "one", Two: "two"}))
+	logger.Info("gorm data: product", zap.Any("ads", Product{}))
+	logger.Error("test", zap.Any("foo", foo{One: "one", Two: "two"}), zap.String("asd", "123"))
+
+	logger.Info("Success..",
+		zap.String("statusCode", "200"))
+}
+
